6af54f7ff161db392092f577a78f2b961fb4aa25
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-06 21:10:46 -0500

    Add commonCss utility file

---
+++ src/app/utilz/commonCss.ts
@@ -0,0 +1,7 @@
+// export const cssGlass ="h-full w-full bg-gray-0 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-100";
+
+
+export function cssGlass(): string{
+    return "h-full w-full bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border-0.5 border-gray-100";
+}
+
