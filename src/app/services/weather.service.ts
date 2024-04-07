53e81364f2e7e049aa5c52f9d85879eea6651548
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-07 14:34:35 -0500

    Add API integration for weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { constructAPI } from '../core/constants/env';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,11 @@ export class WeatherService {
   constructor() { }
 
   getWeather(city: string): string{
+
+    const API = constructAPI(city);
+
+
+
     return `The weather in ${city} is 23 degrees celcius`;
   }
 }
