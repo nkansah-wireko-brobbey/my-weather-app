72a7d9c75b9cf14b4fac6fc628dc71775bbc8062
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-07 22:52:42 -0500

    Update LocationService to use LocationsAPIResponse and add suggestion to suggestionStore

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -2,22 +2,23 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, tap } from 'rxjs';
 import { constructAPI } from '../core/constants/env';
-import { APIResponse } from '../core/models/cityInfo.model';
+import { LocationsAPIResponse } from '../core/models/cityInfo.model';
+import { SuggesstionstorageService } from './suggesstionstorage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private suggestionStore: SuggesstionstorageService) {
    }
 
-   getLocationByCity(city: string): Observable<APIResponse> {
+   getLocationByCity(city: string): Observable<LocationsAPIResponse> {
     const API = constructAPI(city);
 
-     return this.http.get<APIResponse>(API).pipe(
-        tap((response: APIResponse) => {
-          console.log(response);
+     return this.http.get<LocationsAPIResponse>(API).pipe(
+        tap((response: LocationsAPIResponse) => {
+          this.suggestionStore.addSuggestion(response);
         })
       );
    }
