ac951163bda74ec68a8c7514ec747156e7fbb690
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-08 22:35:12 -0500

    Add City type to cityInfo.model.ts

--- src/app/core/models/cityInfo.model.ts
+++ src/app/core/models/cityInfo.model.ts
@@ -10,6 +10,8 @@ interface CityInfo {
 // Example API response interface
 export type LocationsAPIResponse = CityInfo[];
 
+export type City = CityInfo;
+
 
 interface CurrentWeatherRequest{
     latittude: number;
