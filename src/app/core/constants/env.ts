08367dd7dbf51767305a0a6d732fb4fbb9e1b0af
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-08 22:34:50 -0500

    Update API constant names in env.ts

--- src/app/core/constants/env.ts
+++ src/app/core/constants/env.ts
@@ -1,15 +1,17 @@
-const API = 'http://api.openweathermap.org/geo/1.0/direct?limit=5&appid=643cbfb8f3f8c80329b20f9fedcbdb93';
+const LocationAPI = 'http://api.openweathermap.org/geo/1.0/direct?limit=5&appid=643cbfb8f3f8c80329b20f9fedcbdb93';
+
+const WeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?appid=643cbfb8f3f8c80329b20f9fedcbdb93'
 
 export const environment = {
     production: false,
-    API
+    API: LocationAPI
     };
 
 
 export const constructLocationAPI = (city: string) => {
-    return `${API}&q=${city}`;
+    return `${LocationAPI}&q=${city}`;
 }
 
 export const constructWeatherAPI = (lat: number, lon: number) => {
-    return `${API}&lat=${lat}&lon=${lon}`;
+    return `${WeatherAPI}&lat=${lat}&lon=${lon}`;
 }
