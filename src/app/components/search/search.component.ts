9f70243eecd0ca04886828731348b6c5b73e818e
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-08 22:35:01 -0500

    Add WeatherService to search component constructor

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LocationService } from '../../services/location.service';
 import { SuggesstionstorageService } from '../../services/suggesstionstorage.service';
+import { City } from '../../core/models/cityInfo.model';
 
 @Component({
   selector: 'app-search',
@@ -18,13 +19,39 @@ export class SearchComponent {
 
   suggestions$ = computed(() => this.suggestionStorage.suggestions());
 
-  constructor(private _location: LocationService, private suggestionStorage: SuggesstionstorageService) { }
+  constructor(
+    private _location: LocationService, 
+    private suggestionStorage: SuggesstionstorageService,
+    private _weatherService: WeatherService
+  ) { }
 
   getLocation(){
     if(this.city.trim() === '') return;
     this._location.getLocationByCity(this.city).subscribe((response) => {
       console.log(response);
     });
+  }
+
+
+  getWeather(selectedCity: City){
+    if(!selectedCity) return;
+
+    this.setCity(selectedCity.name);
+    this.clearSuggestions();
+
+    this._weatherService.getWeather(selectedCity.lat, selectedCity.lon).subscribe((response) => {
+      console.log(response);
+    });
+    
+    
+  }
+
+  clearSuggestions(){
+    this.suggestionStorage.clearSuggestions();
+  }
+
+  setCity(city: string){
+    this.city = city;
   }
 
 }
