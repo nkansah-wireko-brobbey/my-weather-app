b2c7be107b2a16fbf4da501db35ced768644b8d9
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-08 22:53:00 -0500

    Add WeatherdatastorageService to handle weather data storage

---
+++ src/app/services/weatherdatastorage.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, signal, effect } from '@angular/core';
+import { WeatherData } from '../core/models/weather.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherdatastorageService {
+
+  private initialValue: WeatherData = {} as WeatherData;
+
+  private weatherData = signal<WeatherData>(
+    this.initialValue
+  );
+
+  constructor() { }
+ 
+
+ addWeatherData(weatherData: WeatherData){
+    this.weatherData.update((value: WeatherData) => weatherData);
+
+  }
+
+  clearWeatherData(){
+    this.weatherData.update((value: WeatherData) => this.initialValue);
+  }
+
+}
